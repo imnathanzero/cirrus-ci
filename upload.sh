6d41e486ce0239dc07337d0243599b409dca4ac5
Nathan Andrew <nathannxx@proton.me>
2025-01-21 05:52:54 +0700

    well you know

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/RMX1941/Havoc-OS*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/RMX1941/ && ls Havoc-OS*.zip)"
-      rclone copy ~/rom/out/target/product/RMX1941/Havoc-OS*.zip nath:havoc -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/RMX1941/ && ls Havoc-OS*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/RMX1941/xdCLO_*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/RMX1941/ && ls xdCLO_*.zip)"
+      rclone copy ~/rom/out/target/product/RMX1941/xdCLO_*.zip nath:havoc -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/RMX1941/ && ls xdCLO_*.zip) Uploaded Successfully!"
 fi
